797d2195c1d9bb0d2433e78d7452565168680e19
Alsh0815 <alshavin0815@gmail.com>
2021-07-02 17:15:38 +0900

    Fixed some source code.

--- gbp.score.calculator/script/onload.js
+++ gbp.score.calculator/script/onload.js
@@ -3,25 +3,46 @@ window.onload = function init() {
         var select_bgm = document.getElementById("input_bgm");
         for (i=0; i<data.BGM.length; i++) {
             var option = document.createElement("option");
-            option.text = data.BGM[i][0];
-            option.value = data.BGM[i][1];
+            option.text = data.BGM[i].name;
+            option.value = data.BGM[i].id;
             select_bgm.appendChild(option);
         }
     });
     $.getJSON('database/SKILL.json', (data) => {
         var select_skill1 = document.getElementById("input_skill1");
-        var select_skill2 = document.getElementById("input_skill2");
-        var select_skill3 = document.getElementById("input_skill3");
-        var select_skill4 = document.getElementById("input_skill4");
-        var select_skill5 = document.getElementById("input_skill5");
-        var list1 = [select_skill1,select_skill2,select_skill3,select_skill4,select_skill5];
         for (i=0; i<data.SKILL.length; i++) {
-            for (ii=0; ii<list1.length; ii++) {
-                var option = document.createElement("option");
-                option.text = data.SKILL[i][0];
-                option.value = data.SKILL[i][1];
-                list1[ii].appendChild(option);
-            }
+            var option = document.createElement("option");
+            option.text = data.SKILL[i][0];
+            option.value = data.SKILL[i][1];
+            select_skill1.appendChild(option);
+        }
+        var select_skill2 = document.getElementById("input_skill2");
+        for (i=0; i<data.SKILL.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.SKILL[i][0];
+            option.value = data.SKILL[i][1];
+            select_skill2.appendChild(option);
+        }
+        var select_skill3 = document.getElementById("input_skill3");
+        for (i=0; i<data.SKILL.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.SKILL[i][0];
+            option.value = data.SKILL[i][1];
+            select_skill3.appendChild(option);
+        }
+        var select_skill4 = document.getElementById("input_skill4");
+        for (i=0; i<data.SKILL.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.SKILL[i][0];
+            option.value = data.SKILL[i][1];
+            select_skill4.appendChild(option);
+        }
+        var select_skill5 = document.getElementById("input_skill5");
+        for (i=0; i<data.SKILL.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.SKILL[i][0];
+            option.value = data.SKILL[i][1];
+            select_skill5.appendChild(option);
         }
         var select_skilltime1 = document.getElementById("input_skillTime1");
         var select_skilltime2 = document.getElementById("input_skillTime2");
