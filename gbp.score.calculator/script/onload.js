e01b7942f17c5cdd384ff19ab58d89f9ba4948b2
Alsh0815 <alshavin0815@gmail.com>
2021-07-02 17:25:46 +0900

    .

--- gbp.score.calculator/script/onload.js
+++ gbp.score.calculator/script/onload.js
@@ -45,18 +45,38 @@ window.onload = function init() {
             select_skill5.appendChild(option);
         }
         var select_skilltime1 = document.getElementById("input_skillTime1");
-        var select_skilltime2 = document.getElementById("input_skillTime2");
-        var select_skilltime3 = document.getElementById("input_skillTime3");
-        var select_skilltime4 = document.getElementById("input_skillTime4");
-        var select_skilltime5 = document.getElementById("input_skillTime5");
         for (i=0; i<data.TIME.length; i++) {
             var option = document.createElement("option");
             option.text = data.TIME[i];
             option.value = data.TIME[i];
             select_skilltime1.appendChild(option);
+        }
+        var select_skilltime2 = document.getElementById("input_skillTime2");
+        for (i=0; i<data.TIME.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.TIME[i];
+            option.value = data.TIME[i];
             select_skilltime2.appendChild(option);
+        }
+        var select_skilltime3 = document.getElementById("input_skillTime3");
+        for (i=0; i<data.TIME.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.TIME[i];
+            option.value = data.TIME[i];
             select_skilltime3.appendChild(option);
+        }
+        var select_skilltime4 = document.getElementById("input_skillTime4");
+        for (i=0; i<data.TIME.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.TIME[i];
+            option.value = data.TIME[i];
             select_skilltime4.appendChild(option);
+        }
+        var select_skilltime5 = document.getElementById("input_skillTime5");
+        for (i=0; i<data.TIME.length; i++) {
+            var option = document.createElement("option");
+            option.text = data.TIME[i];
+            option.value = data.TIME[i];
             select_skilltime5.appendChild(option);
         }
     });
